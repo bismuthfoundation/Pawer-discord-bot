c32cf93d95363d4d4a2dfa714b6e832b77992c07
iyomisc <36338016+iyomisc@users.noreply.github.com>
2019-06-11 16:06:58 +0200

    Add Vinex ETH Pair.

--- cogs/bismuth.py
+++ cogs/bismuth.py
@@ -80,12 +80,11 @@ class Bismuth:
             if market["market"]["identifier"] in MARKETS:
                 if market["target"] == "BTC":
                     prices.append("▸ {:0.8f} BTC or {:0.2f} USD on {}".format(market["last"], market["converted_last"]["usd"], market["market"]["name"]))
+                if market["target"] == "ETH":
+                    prices.append("▸ {:0.8f} ETH or {:0.2f} USD on {}".format(market["last"], market["converted_last"]["usd"], market["market"]["name"]))
         prices = "\n".join(prices)
         # await client.send_message(discord.Object(id='502494064420061184'), "Bitcoin price is: " + value)
         await self.bot.say("{} price is:\n{}".format(EMOJIS['Bismuth'], prices))
-
-
-
 
 
     @commands.command(name='deposit', brief="Shows or creates a BIS deposit address", pass_context=True)
