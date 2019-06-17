b1c5870d9005b754e455929cbff7474c95533000
iyomisc <36338016+iyomisc@users.noreply.github.com>
2019-06-17 15:27:47 +0200

    test off chain signature of message

--- cogs/bismuth.py
+++ cogs/bismuth.py
@@ -372,6 +372,35 @@ class Bismuth:
             await self.bot.add_reaction(ctx.message, '👎')  # Thumb down
             await self.bot.say("Error {}".format(e))
 
+    @commands.command(name='sign', brief="Sign a message from your wallet, for off-chain use", pass_context=True)
+    async def sign(self, ctx, message: str):
+        try:
+            user = User(ctx.message.author.id)
+            user_info = user.info()
+            if user_info and user_info['address']:
+                # User exists and validated the terms, has an address
+                # Make sure balance is enough
+                res = user.sign_message(message)
+                sign = res['sign']
+                # print("sign", sign)
+                if sign:
+                    # answer by reaction not to pollute
+                    await self.bot.add_reaction(ctx.message, '👍')  # Thumb up
+                    await self.bot.say("Signature is `{}`.".format(sign))
+                else:
+                    await self.bot.add_reaction(ctx.message, '👎')  # Thumb down
+                    await self.bot.say("Error {}".format(res['error']))
+                return
+            # Depending on channel, say or send PM
+            em = discord.Embed(description=DISCLAIMER, colour=discord.Colour.red())
+            em.set_author(name="You have to create your address first:")
+            await self.bot.say(embed=em)
+        except Exception as e:
+            print(str(e))
+            # Send a PM to the sender or answer if dedicated channel
+            await self.bot.add_reaction(ctx.message, '👎')  # Thumb down
+            await self.bot.say("Error {}".format(e)) 
+
     @commands.command(name='accept', brief="Accept the Pawer terms, run deposit first", pass_context=True)
     async def accept(self, ctx):
         user = User(ctx.message.author.id)
