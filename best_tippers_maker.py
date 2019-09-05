b7de48c69565041eca2a68ce2ddbaf31ca9f3b63
EggPool <33421807+EggPool@users.noreply.github.com>
2019-09-05 15:53:27 +0200

    Raise tip board to top 10

--- best_tippers_maker.py
+++ best_tippers_maker.py
@@ -11,7 +11,7 @@ for tip_type in ["tip", "rain", "eggrain"]:
     data[tip_type] = {}
     for address in ["from", "to"]:
         cursor.execute("select sum(amount), count(amount),  id from tips join users where type='{}' and address={}_"
-                       "address group by {}_address order by sum(amount) desc limit 5;".format(tip_type, address, address))
+                       "address group by {}_address order by sum(amount) desc limit 10;".format(tip_type, address, address))
         data[tip_type][address] = cursor.fetchall()
 
 with open("data/tips.json", "w") as f:
