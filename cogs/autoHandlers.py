0ba92161e0b1352ffbcfd0a7483a54bbe0abfc8d
AnimusXCASH <45211126+AnimusXCASH@users.noreply.github.com>
2020-09-27 14:27:11 +0300

    Integration of on_command_error listener

---
+++ cogs/autoHandlers.py
@@ -0,0 +1,44 @@
+"""
+COG: Dealing with automatic messages to Discord Community and single users.
+"""
+
+import os
+import sys
+
+from discord.ext import commands
+from modules.config import CONFIG, EMOJIS, SHORTCUTS
+
+
+class AutoCommands:
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def system_message(self, ctx, error, destination: int, handled: bool):
+        """
+        Send the error to selected destination
+        """
+        if destination == 1:  # To sys channel
+            dest = await self.bot.get_channel(id=CONFIG['bot_channel'][0])
+        elif destination == 2:  # To ctx channel
+            dest = ctx.message.channel
+        elif destination == 3:  # To author
+            dest = ctx.message.author
+
+        if handled:
+            await dest.send(content=f"Command Executed: {ctx.message.content}\n"
+                                    f"User: {ctx.message.author} ({ctx.message.author.id})\n"
+                                    f"Error: {error}")
+        else:
+            await dest.send(
+                content=f":warning: ***Unhandled Error***:warning: \nCommand Executed:{ctx.message.content}\n"
+                        f"User: {ctx.message.author} ({ctx.message.author.id})\n"
+                        f"Channel: {ctx.message.channel}"
+                        f"Error: {error}")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await self.system_message(ctx=ctx, error=error, destination=3, handled=True)
+
+        else:
+            await self.system_message(ctx=ctx, error=error, destination=1, handled=True)
